8e3943f008620adf5f2f33bdad434558d92c85ae
ĐɆ₦₳₱ØⱠɆ₴ <145853598+DeNapoles@users.noreply.github.com>
2026-07-12 19:54:44 +0100

    Update fetch-data.js

--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -136,6 +136,7 @@ async function fetchFlyweelCampaigns(store) {
           arguments: {
             queries: [
               {
+                dataSource: 'meta',
                 ad_account_id: store.flyweel_ad_account_id,
                 metrics: ['spend', 'revenue', 'roas', 'cpa', 'ctr', 'frequency'],
                 dimensions: ['campaign_name'],
